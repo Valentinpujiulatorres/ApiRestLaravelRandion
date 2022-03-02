d75d5f5f3d21d14b4393791aa6b54d02c03075e2
Valentin <vpujiula@cifpfbmoll.eu>
2022-03-03 00:12:32 +0100

    Fixed Laravel Bug on Boolean

--- public/js/Api.js
+++ public/js/Api.js
@@ -135,6 +135,7 @@ function renderFormularioCrear() {
                 
                 <div class="mb-3">
                     <label for="available" class="form-label">Available</label>
+                    <input type="hidden"  value="0" id="available" />
                     <input type="checkbox"  value="1" id="available" />
                 </div>
                 <button type="submit" onclick="NewRecord()" class="btn btn-primary"> Crear </button>
@@ -159,7 +160,8 @@ function renderFormularioEditar($id) {
                 
                 <div class="mb-3">
                     <label for="available" class="form-label">Available</label>
-                    <input type="checkbox" value='1' id="available" />
+                    <input type="hidden"  value="0" id="available" />
+                    <input type="checkbox" value="1" id="available" />
                 </div>
                 <button type="submit" onclick="edit(${EditID})" class="btn btn-success"> Editar </button>
                
