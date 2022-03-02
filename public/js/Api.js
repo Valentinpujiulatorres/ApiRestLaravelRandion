c32f14a5da2050574f2a42b94e273252071b57b0
Valentin <vpujiula@cifpfbmoll.eu>
2022-03-02 17:11:31 +0100

    update

--- public/js/Api.js
+++ public/js/Api.js
@@ -38,7 +38,6 @@ async function getData(url) {
         <td>${element.price}</td>
         <td>${element.available}</td>
         <td><button id='${element.id}' class="btn btn-warning" onclick="destroy(this.id)">Delete</button></td>
-        
         </tr>`
     });
 
