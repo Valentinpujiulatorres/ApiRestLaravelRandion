d96b1577b4ce5320bcccfb41ba2896541e4606f7
Valentin <vpujiula@cifpfbmoll.eu>
2022-03-02 23:58:37 +0100

    MS

--- public/js/Api.js
+++ public/js/Api.js
@@ -117,7 +117,7 @@ function NewRecord() {
     window.location.reload();
 }
 
-//-------------------------------------------------------------------
+
 
 function renderFormularioCrear() {
     document.getElementById('formulario').innerHTML = `<div class="mb-3">
@@ -135,14 +135,12 @@ function renderFormularioCrear() {
                 
                 <div class="mb-3">
                     <label for="available" class="form-label">Available</label>
-                    <input type="checkbox" value='true' id="available"  />
+                    <input type="checkbox" checked="1" id="available" />
                 </div>
                 <button type="submit" onclick="NewRecord()" class="btn btn-primary"> Crear </button>
                
                 `;
 }
-
-//-------------------------------------------------------------------
 
 function renderFormularioEditar($id) {
     let EditID = $id
@@ -161,7 +159,7 @@ function renderFormularioEditar($id) {
                 
                 <div class="mb-3">
                     <label for="available" class="form-label">Available</label>
-                    <input type="checkbox" value='true' id="available" />
+                    <input type="checkbox" checked="1" id="available" />
                 </div>
                 <button type="submit" onclick="edit(${EditID})" class="btn btn-success"> Editar </button>
                
